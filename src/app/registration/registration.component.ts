ab7841ca768e66582511f7ce72e8c570efcee3b7
AdvaithInka <advaith@inka.co.kr>
2024-09-13 15:58:58 +0530

    pushed pallycon webpage UI code

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./styles.css']
+})
+export class RegistrationComponent {
+
+}
